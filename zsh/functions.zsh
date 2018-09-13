e4a6e7d4a41f563df93db17abff5b7a40d26701f
Masoud <mpoloton@gmail.com>
2018-09-13 23:44:40 +0200

    added pdfextract function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -96,3 +96,18 @@ function show_mod_parameter_info ()
   done < <(cut -d' ' -f1 /proc/modules | sort)
 }
 
+
+function pdfpextr()
+{
+    # this function uses 3 arguments:
+    #     $1 is the first page of the range to extract
+    #     $2 is the last page of the range to extract
+    #     $3 is the input file
+    #     output file will be named "inputfile_pXX-pYY.pdf"
+    gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER \
+       -dFirstPage=${1} \
+       -dLastPage=${2} \
+       -sOutputFile=${3%.pdf}_p${1}-p${2}.pdf \
+       ${3}
+}
+
